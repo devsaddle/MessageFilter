44d783cc5ff2bad275fc0648f98752b471fb6a70
Yuan <mayuan607@gmail.com>
2017-10-01 23:23:31 +0800

    Initial Commit

---
+++ MessageFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageFilter
+//
+//  Created by 马远 on 2017/10/1.
+//  Copyright © 2017年 马远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageFilter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MessageFilter
+//
+//  Created by 马远 on 2017/10/1.
+//  Copyright © 2017年 马远. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
